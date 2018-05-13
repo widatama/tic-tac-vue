2b32d50258ccafe16705279bc44fe5baa101f7fc
Danu Widatama <widatama@gmail.com>
2018-05-13 16:09:32 +0700

    Set up workbox

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
-const Webpack = require('webpack');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackStylish = require('webpack-stylish');
 
 const webpackConfig = require('./webpack.config');
 const appConfig = require('./app.config');
+
+const WorkboxPluginConfig = {
+  // globDirectory: path.resolve('', `./${appConfig.paths.dist.path}`),
+  // globPatterns: ['**/*.{js,html,css}'],
+  swDest: path.resolve('', `./${appConfig.paths.dist.path}/sw.js`),
+  clientsClaim: true,
+  skipWaiting: true,
+};
 
 webpackConfig.mode = 'production';
 webpackConfig.stats = 'none';
@@ -20,6 +28,7 @@ webpackConfig.plugins = (webpackConfig.plugins || []).concat([
   new OptimizeCSSPlugin({
     cssProcessorOptions: { discardComments: { removeAll: true } },
   }),
+  new WorkboxPlugin.GenerateSW(WorkboxPluginConfig),
   new WebpackStylish(),
 ]);
 
