e62734fcb6c62ab3e5ec547e5916c7dd6ac6089e
Danu Widatama <widatama@gmail.com>
2022-12-18 22:47:43 +0700

    Migrate vuex store to ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,16 @@ import actions from './actions';
 import getters from './getters';
 import mutations from './mutations';
 
+const state = {
+  gameTurn: getFirstTurn(),
+  cells: generateCells(9),
+  winner: '',
+};
+
+export type State = typeof state;
+
 export default createStore({
-  state: {
-    gameTurn: getFirstTurn(),
-    cells: generateCells(9),
-    winner: '',
-  },
+  state,
   getters,
   mutations,
   actions,
--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,24 +1,26 @@
 import { getFirstTurn, generateCells, players } from '@/modules/engine';
+import type { Cell } from '@/modules/engine';
+import type { State } from './index';
 
-const NEW_GAME = (state) => {
+const NEW_GAME = (state: State) => {
   state.cells = generateCells(9);
   state.gameTurn = getFirstTurn();
   state.winner = '';
 };
 
-const UPDATE_CELLS = (state, newCell) => {
+const UPDATE_CELLS = (state: State, newCell: Cell) => {
   state.cells[newCell.pos - 1] = newCell;
 };
 
-const UPDATE_TURN = (state) => {
+const UPDATE_TURN = (state: State) => {
   if (state.gameTurn === players[0]) {
-    state.gameTurn = players[1];
+    [, state.gameTurn] = players;
   } else if (state.gameTurn === players[1]) {
-    state.gameTurn = players[0];
+    [state.gameTurn] = players;
   }
 };
 
-const UPDATE_WINNER = (state, newWinner) => {
+const UPDATE_WINNER = (state: State, newWinner: string) => {
   state.winner = newWinner;
 };
 
@@ -28,5 +30,7 @@ const mutations = {
   UPDATE_TURN,
   UPDATE_WINNER,
 };
+
+export type Mutations = typeof mutations;
 
 export default mutations;
